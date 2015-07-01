ac41fe8ef2a296f7e4c3a563d8b19f9cf9b023a7
Junyu <beta168921@gmail.com>
2015-07-01 00:45:50 -0400

    main python

--- gui/login_main.py
+++ gui/login_main.py
@@ -9,7 +9,15 @@ class TestDialog(QDialog,login.Ui_Dialog):
 		self.setupUi(self)
 
 		# ui = login.Ui_Dialog()
-		print self.lineEdit_username.text()
+
+		# username = self.lineEdit_username
+		# signin = self.pushButton_signin
+		# print signin
+		self.connect(self.pushButton_signin, SIGNAL("clicked()"), self.signin_button_click)
+	def signin_button_click(self):
+		username = self.lineEdit_username.text()
+		password = self.lineEdit_password.text()
+		print "username: ",username,"\npassword: ", password
 
   
 app=QApplication(sys.argv)  
