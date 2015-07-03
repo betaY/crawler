4ffdb99498380b684dc78b5376d2d7e26abaa648
Junyu <beta168921@gmail.com>
2015-07-03 00:20:52 -0400

    update

--- gui/kit/main.cpp
+++ gui/kit/main.cpp
@@ -17,7 +17,7 @@ int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
-    QNetworkRequest req(QUrl("http://www.google.com"));
+    QNetworkRequest req(QUrl("http://www.pixiv.net"));
     req.setHeader(QNetworkRequest::UserAgentHeader, "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.130 Safari/537.36");
 
     QNetworkAccessManager man;
