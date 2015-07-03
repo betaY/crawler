b9705b754b0233819bef96ca4fb1b2d9693aefdd
beta <beta168921@gmail.com>
2015-07-02 21:41:17 -0400

    test

--- gui/qttest/main.cpp
+++ gui/qttest/main.cpp
@@ -8,21 +8,31 @@
 #include <QNetworkReply>
 #include <QUrl>
 
-void request() {
-    QNetworkAccessManager* manage = new QNetworkAccessManager();
-    QNetworkReply* reply = manage->get(QNetworkRequest(QUrl("http://www.google.com")));
-    QByteArray data = reply->readAll();
-    QString str(data);
+//void request() {
+//    QNetworkAccessManager* manage = new QNetworkAccessManager();
+//    QNetworkReply* response = manage->get(QNetworkRequest(QUrl("http://www.google.com")));
+//    QByteArray data = reply->readAll();
+//    QString str(data);
 
-    QTextStream qerr(stderr);
-    qerr << "+++++ start ++++++\n";
-    qerr << str;
-    qerr << "\n+++++ end +++++";
-}
+//    connect(response,SIGNAL(finished()),&event,SLOT(quit()));
+//    event.exec();
+//    QString html = response->readAll();
+
+//    QTextStream qerr(stderr);
+//    qerr << "+++++ start ++++++\n";
+//    qerr << html;
+//    qerr << "\n+++++ end +++++";
+//}
 
 int main(int argc, char *argv[])
 {
-    request();
+    QNetworkAccessManager *manager = new QNetworkAccessManager();
+//    QNetworkReply* response = manage->get(QNetworkRequest(QUrl("http://www.google.com")));
+//    connect(manager, SIGNAL(finished(response)), this, SLOT(replyFinished(response)));
+    connect(manager, SIGNAL(finished(QNetworkReply*)),
+            this, SLOT(replyFinished(QNetworkReply*)));
+    manager->get(QNetworkRequest(QUrl("http://www.google.com")));
+
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
---
+++ gui/test/test.pro
@@ -0,0 +1,20 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2015-07-02T13:36:52
+#
+#-------------------------------------------------
+
+QT       += core network
+
+QT       -= gui
+
+TARGET = test
+CONFIG   += console
+CONFIG   -= app_bundle
+
+TEMPLATE = app
+
+
+SOURCES += main.cpp
+
+HEADERS +=
