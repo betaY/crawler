56e33866156a5eca196af12a81ef0621af11a87e
beta <beta168921@gmail.com>
2015-07-04 22:34:55 -0400

    update

--- gui/kit/main.cpp
+++ gui/kit/main.cpp
@@ -17,11 +17,15 @@ int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
-    QNetworkRequest req(QUrl("http://www.pixiv.net"));
+    QNetworkRequest req(QUrl("https://www.secure.pixiv.net/login.php?return_to=%2F"));
+    // http://www.pixiv.net/
     req.setHeader(QNetworkRequest::UserAgentHeader, "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.130 Safari/537.36");
 
     QNetworkAccessManager man;
 //    QNetworkCookieJar *cookies = new QNetworkCookieJar();
+    /*Cookie:p_ab_id=7; login_ever=yes; hide_premium_tutorial_modal=1434740779; device_token=ef878b5484ef1a415161e577a9cfb6bd; module_orders_mypage=%5B%7B%22name%22%3A%22everyone_new_illusts%22%2C%22visible%22%3Atrue%7D%2C%7B%22name%22%3A%22spotlight%22%2C%22visible%22%3Atrue%7D%2C%7B%22name%22%3A%22featured_tags%22%2C%22visible%22%3Atrue%7D%2C%7B%22name%22%3A%22contests%22%2C%22visible%22%3Atrue%7D%2C%7B%22name%22%3A%22following_new_illusts%22%2C%22visible%22%3Atrue%7D%2C%7B%22name%22%3A%22mypixiv_new_illusts%22%2C%22visible%22%3Atrue%7D%2C%7B%22name%22%3A%22booth_follow_items%22%2C%22visible%22%3Atrue%7D%5D; PHPSESSID=0cb6a9cf98aba409e6d4ea6a01daa6b4
+
+ */
 //    cookies->insertCookie(QNetworkCookie("id", "227f9c4f9e0300a1||t=1414033339|et=730|cs=002213fd48d69442b24638839a"));
 //    cookies->insertCookie(QNetworkCookie("login_ever", "yes"));
 //    cookies->insertCookie(QNetworkCookie("module_orders_mypage    ", "%5B%7B%22name%22%3A%22everyone_new_illusts%22%2C%22visible%22%3Atrue%7D%2C%7B%22name%22%3A%22spotlight%22%2C%22visible%22%3Atrue%7D%2C%7B%22name%22%3A%22featured_tags%22%2C%22visible%22%3Atrue%7D%2C%7B%22name%22%3A%22contests%22%2C%22visible%22%3Atrue%7D%2C%7B%22name%22%3A%22following_new_illusts%22%2C%22visible%22%3Atrue%7D%2C%7B%22name%22%3A%22mypixiv_new_illusts%22%2C%22visible%22%3Atrue%7D%2C%7B%22name%22%3A%22booth_follow_items%22%2C%22visible%22%3Atrue%7D%5D"));
@@ -48,7 +52,7 @@ int main(int argc, char *argv[])
 
         for (int i = 0; i < headers.size(); ++i)
         {
-            //qerr << "HEADER: " << headers[i].first << " CONTENT: " << headers[i].second << "\n";
+            qerr << "HEADER: " << headers[i].first << " CONTENT: " << headers[i].second << "\n";
         }
 
         QByteArray response = reply->readAll();
