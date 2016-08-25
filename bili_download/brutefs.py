124d85ea5bbe16f0b04d10c71b80d19d522dc1e3
Junyu <beta168921@gmail.com>
2016-08-24 22:41:25 -0400

    update

--- bili_download/brutefs.py
+++ bili_download/brutefs.py
@@ -1,22 +1,32 @@
-import hashlib
+import hashlib, sys, time
 
 
 # http://interface.bilibili.com/playurl?cid=6907160&player=1&ts=1471881577&sign=0edc1e6f1a10a570d3a51df5165e83fb
 parm = "cid=6907160&player=1&ts=1471881577"
-salt = [0]*32
+#s = ['f']*32
+total = float.fromhex('ffffffffffffffffffffffffffffffff')
 
 i = 0
+if (len(sys.argv) < 2):
+    i = 0
+else:
+    i = int(sys.argv[1])
 while 1:
-    salt = hex(i)[2:].zfill(16)
+    salt = hex(i)[2:].zfill(32)
     i = i+1
-    if len(salt) > 16:
+    if len(salt) > 32:
         break
     else:
         parmsum = parm + salt
         md5 = hashlib.md5(parmsum).hexdigest()
-        print parmsum + "\t" + md5
+        #print parmsum + "\t" + md5
+        sys.stdout.write("\r%.5f%%%%  i=%d\t\t\t\t%s" % (float(i/total)*10000,i,salt))
+        sys.stdout.flush()
         if (md5 == '0edc1e6f1a10a570d3a51df5165e83fb'):
             print salt
+            f = open('./salt.txt', 'w')
+            f.write(salt+'\n')
+f.close()
 
 
 
